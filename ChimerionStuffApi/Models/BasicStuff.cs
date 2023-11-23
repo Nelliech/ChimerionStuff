231dd44b84effc25e0582386acf0900e6e844e11
Michał Kijewski <m.kijewski142@gmail.com>
2023-11-23 19:24:11 +0100

    Add Property : Manufacturer(string), ProductionDate(DateTime),ProductionPlace(string)

--- ChimerionStuffApi/Models/BasicStuff.cs
+++ ChimerionStuffApi/Models/BasicStuff.cs
@@ -1,9 +1,16 @@
-﻿namespace ChimerionStuffApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChimerionStuffApi.Models
 {
     public abstract class BasicStuff :BasicEntity
     {
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Manufacturer { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public string ProductionPlace { get; set; }
+
 
     }
 }
